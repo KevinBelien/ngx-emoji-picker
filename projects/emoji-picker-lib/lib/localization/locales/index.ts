555a7c96d3cd9ebc495e38e36c9b4b459f6274a6
Kevin Beliën <k3vin.belien@gmail.com>
2024-09-06 23:12:16 +0200

    feat: better way for dealing with library and entry points and locale files

---
+++ projects/emoji-picker-lib/lib/localization/locales/index.ts
@@ -0,0 +1,8 @@
+import deTranslations from './de.json';
+import enTranslations from './en.json';
+import esTranslations from './es.json';
+import frTranslations from './fr.json';
+import itTranslations from './it.json';
+import nlTranslations from './nl.json';
+
+export { deTranslations, enTranslations, esTranslations, frTranslations, itTranslations, nlTranslations };
---
+++ projects/emoji-picker-lib/public-api.ts
@@ -0,0 +1,1 @@
+export const EMOJI_PICKER_UI_KIT = '@chit-chat/ngx-emoji-picker';
