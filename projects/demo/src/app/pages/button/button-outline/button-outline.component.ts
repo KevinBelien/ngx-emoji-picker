4650ec67f5f04196e43257ed721a8ebe43987bc7
Kevin Beliën <k3vin.belien@gmail.com>
2024-09-09 09:06:44 +0200

    chore: theme change for showcase doc

--- projects/demo/src/app/pages/button/button-outline/button-outline.component.ts
+++ projects/demo/src/app/pages/button/button-outline/button-outline.component.ts
@@ -11,4 +11,16 @@ import { ButtonComponent, ButtonType } from '@chit-chat/ngx-emoji-picker/lib/com
 })
 export class ButtonOutlineComponent {
     buttonTypes: ButtonType[] = ['primary', 'success', 'danger', 'warning', 'info', 'contrast'];
+
+    constructor() {
+        window.addEventListener('message', (event) => {
+            const theme = event.data.theme;
+            console.log('gets to angular theme change');
+            if (theme === 'dark') {
+                document.documentElement.setAttribute('data-theme', 'dark');
+            } else {
+                document.documentElement.setAttribute('data-theme', 'light');
+            }
+        });
+    }
 }
